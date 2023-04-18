714bbe3e8fe560448241b5d8f1a2b5cdc702e482
Burak Tırman <89486594+buraktirman@users.noreply.github.com>
2023-04-18 04:58:42 +0300

    Entities completed

---
+++ Backend/Entity/Concrete/SystemStaff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class SystemStaff : Entity
+    {
+        public int UserId { get; set; }
+        public bool StaffStatus { get; set; }
+        public string StaffNumber { get; set; }
+        public virtual User User { get; set; }
+
+        public SystemStaff()
+        {
+        }
+
+        public SystemStaff(int id, int userId, bool staffStatus, string staffNumber) : this()
+
+        {
+            Id = id;
+            UserId = userId;
+            StaffStatus = staffStatus;
+            StaffNumber = staffNumber;
+        }
+    }
+}
---
+++ Backend/Entity/Concrete/UserOperationClaim.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Abstract;
+using Entity.Concrete;
+
+namespace Entity.Concrete;
+    
+public class UserOperationClaim : Entity
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int OperationClaimId { get; set; }
+
+    public virtual User User { get; set; }
+    public virtual OperationClaim OperationClaim { get; set; }
+
+    public UserOperationClaim()
+    {
+    }
+
+    public UserOperationClaim(int id, int userId, int operationClaimId) : base()
+    {
+        Id = id;
+        UserId = userId;
+        OperationClaimId = operationClaimId;
+    }
+}
