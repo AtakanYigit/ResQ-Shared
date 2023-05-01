1c88519b59330ddffffd7cfa93ab090c5a632cf0
Burak Tırman <89486594+buraktirman@users.noreply.github.com>
2023-05-01 23:50:51 +0300

    Data access layer added

---
+++ Backend/DataAccess/Concrete/UserDal.cs
@@ -0,0 +1,28 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class UserDal : EfEntityRepositoryBase<User, BaseDbContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new BaseDbContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                             on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
+            }
+        }
+    }
+}
