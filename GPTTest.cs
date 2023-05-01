e08aecfcdc9c0f21b774fe95a508528795cd3265
Edvin Davulcu <77502473+CentEDO@users.noreply.github.com>
2023-05-01 23:38:24 +0300

    Add files via upload

---
+++ GPTTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using OpenAI.GPT3;
+using OpenAI_API;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var request = Console.ReadLine();   
+        var api = new OpenAI_API.OpenAIAPI("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");
+        var result = await api.Completions.GetCompletion(request);
+        Console.WriteLine(result);
+
+    }
+}
